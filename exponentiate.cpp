c26d2f0974774a781601d0e60a3ab57599b80723
simonfalke <ziqibrandonli@gmail.com>
2023-02-03 15:28:29 +0800

    a

---
+++ exponentiate.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Brandon Li on 3/2/23.
+//
+
+#include <math.h>
+#include <iostream>
+
+int main() {
+    int i;
+    cin >> i;
+
+    for (int g = 0; g < i; g++) {
+
+    }
+}
---
+++ razlika.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Brandon Li on 3/2/23.
+//
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int n;
+    int c;
+
+    cin >> n;
+    cin >> c;
+    int nums[n];
+
+    for (int i = 0; i < n; ++i) {
+        int b;
+        cin >> b;
+        nums[i] = b;
+    }
+
+    sort(nums, nums + n);
+
+    int smallest = -1;
+    for (int i = 0; i < n-1; ++i) {
+        int diff = nums[i+1] - nums[i];
+        if (smallest == -1) smallest = diff;
+        else if (diff < smallest) smallest = diff;
+    }
+
+    int biggest = nums[n-1] - nums[0];
+
+    cout << smallest + biggest;
+}
